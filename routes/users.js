d9aea212e6a2bad4a01759cc05c19176711f48fb
Fredrik Koroma <fekoroma@gmail.com>
2025-07-23 03:59:26 +0100

    Added user login functionality with bcrypt password comparison. Created /users/login route with form and /users/loggedin route to validate credentials against database.

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,73 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 router.get('/register', function (req, res, next) {
     res.render('register.ejs');
 });
 
 router.post('/registered', function (req, res, next) {
-    const bcrypt = require('bcrypt');
-    const saltRounds = 10;
     const plainPassword = req.body.password;
 
-    // saving data in database (just a response for now)
-    res.send(' Hello ' + req.body.first + ' ' + req.body.last + 
-             ' you are now registered! We will send an email to you at ' + 
-             req.body.email);
+    bcrypt.hash(plainPassword, saltRounds, function(err, hashedPassword) {
+        if (err) {
+            return next(err);
+        }
+
+        let result = 'Hello ' + req.body.first + ' ' + req.body.last +
+                     ', you are now registered! We will send an email to you at ' + req.body.email;
+        result += '<br>Your password is: ' + plainPassword + '<br>And your hashed password is: ' + hashedPassword;
+        
+        res.send(result);
+    });
+
+    //List users route
+router.get('/users/list', function (req, res, next) {
+    const sql = 'SELECT first, last, email FROM users';
+    db.query(sql, function (err, results) {
+        if (err) {
+            return next(err);
+        }
+        res.render('userlist.ejs', { users: results });
+    });
 });
 
-// Export the router object so index.js can access it
+    //Login route rendering login form
+router.get('/login', function (req, res, next) {
+  res.render('login.ejs');
+});
+
+//User logged in route (Checks credentials)
+const bcrypt = require('bcrypt');
+
+router.post('/loggedin', function (req, res, next) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const sql = 'SELECT password FROM users WHERE username = ? OR email = ?';
+  db.query(sql, [username, username], function (err, results) {
+    if (err) return next(err);
+
+    if (results.length === 0) {
+      res.send('Login failed: user not found.');
+    } else {
+      const hashedPassword = results[0].password;
+
+      bcrypt.compare(password, hashedPassword, function (err, result) {
+        if (err) return next(err);
+
+        if (result === true) {
+          res.send('Login successful! Welcome back.');
+        } else {
+          res.send('Login failed: incorrect password.');
+        }
+      });
+    }
+  });
+});
+
+
+});
+
 module.exports = router;
